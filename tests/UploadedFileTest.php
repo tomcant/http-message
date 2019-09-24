3d0fbf5ed79abcabe80d22a49987c194bc38efe2
Tom Cant <tomcant@gmail.com>
2019-09-24 08:02:51 +0100

    Test getters in UploadedFile class.

--- tests/UploadedFileTest.php
+++ tests/UploadedFileTest.php
@@ -3,6 +3,8 @@
 namespace Tests\SimpleWeb\Http\Message;
 
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
 use SimpleWeb\Http\Message\Stream;
 use SimpleWeb\Http\Message\UploadedFile;
 
@@ -29,6 +31,40 @@ class UploadedFileTest extends TestCase
 
     public function test_it_can_be_instantiated()
     {
-        $this->assertInstanceOf(UploadedFile::class, $this->uploadedFile);
+        $this->assertInstanceOf(UploadedFileInterface::class, $this->uploadedFile);
+    }
+
+    public function test_it_can_get_the_stream()
+    {
+        $this->assertInstanceOf(StreamInterface::class, $this->uploadedFile->getStream());
+    }
+
+    public function test_it_can_get_the_size()
+    {
+        $size = \rand(0, \PHP_INT_MAX);
+        $uploadedFile = new UploadedFile($this->stream, $size);
+
+        $this->assertEquals($size, $uploadedFile->getSize());
+    }
+
+    public function test_it_can_get_the_error()
+    {
+        $uploadedFile = new UploadedFile($this->stream, null, \UPLOAD_ERR_NO_FILE);
+
+        $this->assertEquals(\UPLOAD_ERR_NO_FILE, $uploadedFile->getError());
+    }
+
+    public function test_it_can_get_the_client_file_name()
+    {
+        $uploadedFile = new UploadedFile($this->stream, null, \UPLOAD_ERR_OK, 'filename.ext');
+
+        $this->assertEquals('filename.ext', $uploadedFile->getClientFilename());
+    }
+
+    public function test_it_can_get_the_client_media_type()
+    {
+        $uploadedFile = new UploadedFile($this->stream, null, \UPLOAD_ERR_OK, null, 'media/type');
+
+        $this->assertEquals('media/type', $uploadedFile->getClientMediaType());
     }
 }
